b8bacc3fbb43b9ceac42e074abc4f16ebb09bb47
EuheniHreben <100788295+EuheniHreben@users.noreply.github.com>
2022-07-22 06:35:56 +0300

    task-6

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,41 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+let currentDrug;
 
-export function createDiv() {}
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+document.addEventListener('mousemove', (e) => {
+  if (currentDrug) {
+    currentDrug.style.top = e.pageY - e.target.offsetHeight / 2 + 'px';
+    currentDrug.style.left = e.pageX - e.target.offsetWidth / 2 + 'px';
+  }
+});
+
+export function createDiv() {
+  const newDiv = document.createElement('div');
+  const randomTop = randomInRange(0, 100);
+  const randomLeft = randomInRange(0, 100);
+  const x = Math.floor(Math.random() * 256);
+  const y = Math.floor(Math.random() * 256);
+  const z = Math.floor(Math.random() * 256);
+  newDiv.classList.add('draggable-div');
+  newDiv.style.width = randomInRange(50, 200) + 'px';
+  newDiv.style.height = randomInRange(50, 200) + 'px';
+  newDiv.style.position = 'absolute';
+  newDiv.style.top = randomTop + '%';
+  newDiv.style.left = randomLeft + '%';
+  newDiv.style.background = 'rgb(' + x + ',' + y + ',' + z + ')';
+
+  newDiv.addEventListener('mousedown', (e) => {
+    currentDrug = newDiv;
+  });
+
+  newDiv.addEventListener('mouseup', () => (currentDrug = false));
+  return newDiv;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
